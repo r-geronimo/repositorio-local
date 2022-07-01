9349f3c9973080dd439f65ee930723d7f9c53162
ronny_ivan@hotmail.com <70739139+r-geronimo@users.noreply.github.com>
2022-06-30 22:25:48 -0400

    Tarea 2 Completa

--- js/Tarea_2.js
+++ js/Tarea_2.js
@@ -93,16 +93,52 @@ console.log("arreglo elementos impares = ", arreglo2);
 //3 - Escriba una función flecha del tipo campo público de clase que tome por argumento un nombre y retorne el mensaje "Hola nombre". Usar template string.
 console.log("Ejercicio 3");
 
+const saludar = nombre => console.log(`Hola ${nombre}`);
+var nombre = "ronny"
+var nombre2 = "ivan"
+saludar(nombre);
+
+saludar(nombre2);
+
 
 //4 - Haga una función flecha que calcule el área de un círculo. El valor de pi debe ser constante.
 console.log("Ejercicio 4");
+
+const pi = 3.1416;
+r = 2;
+
+const AreaCirculo = r => console.log(`El Area de un círculo de radio ${r} es `,pi*r*r,"cm2");
+
+AreaCirculo(r);
 
 
 //5 - Utilice de destructuring para crear dos variable que almacenen los primeros dos elementos del arreglo resultante del ejercicio 2. 
 console.log("Ejercicio 5");
 
+console.log("Resultado Arreglo del Ejercicio 2 = ",arreglo2);
+
+var valores = {x: 5, y:15, z:25};
+
+function display(valores){
+    let {x, y} = valores;
+    console.log(`El primer elemento es ${x} y el segundo elemento es ${y}`);
+}
+
+display(valores);
+
+
 
 //6 - Cree una clase llamada persona con dos atributos : nombre y apellido. Luego cree otra clase llama empleado que herede de Persona y tenga un atributo adicional llamado salario.
 console.log("Ejercicio 6");
+
+var persona = { 
+    nombre: "Ronny",
+    apellido: "Gerónimo"
+}
+
+console.log(persona);
+var empleado = {...persona, salario: 100000 };
+
+console.log(empleado);
 
 
